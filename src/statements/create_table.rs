f8989996a2b6fa67d9ca4cea72a844a2e0d02c8e
Joshua Becnel <jcbecnel@gmail.com>
2024-04-02 11:32:54 -0500

    fixed data length hashmap

--- src/statements/create_table.rs
+++ src/statements/create_table.rs
@@ -11,22 +11,34 @@ pub trait Create {
     ) -> Result<(), OracleSqlToolsError>;
 }
 
+macro_rules! compare_data_length {
+    ($varchar_col_size:ident, $val:ident, $x:ident) => {
+        match $varchar_col_size.get(&$x) {
+            Some(prev_val) => {
+                if $val.to_string().len() > *prev_val { $varchar_col_size.insert($x, $val.to_string().len()) }
+                else { continue }
+            },
+            None => $varchar_col_size.insert($x, $val.to_string().len()),
+        }
+    };
+}
+
 impl Create for Vec<Vec<FormattedData>> {
     fn create_table(
         &self, table_name: &str, data_type_indexes: &DatatypeIndexes, conn: &Connection
     ) -> Result<(), OracleSqlToolsError> {
         if self.len() <= 1 { return Err(OracleSqlToolsError::NoData); }
 
-        let mut varchar_col_size = HashMap::new();
+        let mut varchar_col_size: HashMap<usize, usize> = HashMap::new();
         for x in 0..self[0].len() {
             if !data_type_indexes.is_varchar.contains(&x) { continue; };
             for y in 1..self.len() {
                 match &self[y][x] {
-                    FormattedData::STRING(val) => varchar_col_size.insert(x, val.len()),
-                    FormattedData::INT(val) => varchar_col_size.insert(x, val.to_string().len()),
-                    FormattedData::FLOAT(val) => varchar_col_size.insert(x, val.to_string().len()),
-                    FormattedData::DATE(val) => varchar_col_size.insert(x, val.to_string().len()),
-                    FormattedData::EMPTY => varchar_col_size.insert(x, 0 as usize),
+                    FormattedData::STRING(val) => compare_data_length!(varchar_col_size, val, x),
+                    FormattedData::INT(val) => compare_data_length!(varchar_col_size, val, x),
+                    FormattedData::FLOAT(val) => compare_data_length!(varchar_col_size, val, x),
+                    FormattedData::DATE(val) => compare_data_length!(varchar_col_size, val, x),
+                    FormattedData::EMPTY => { let val = 0 as usize; compare_data_length!(varchar_col_size, val, x) },
                 };
             }
         }
@@ -48,7 +60,6 @@ impl Create for Vec<Vec<FormattedData>> {
         for (i, col_header) in self[0].to_string().iter().enumerate() {
             col_names.push(format!("{} {}", remove_invalid_chars(col_header), sql_data_types[i]))
         }
-
         let create_table_stmt = format!("CREATE TABLE {} ({})", table_name, col_names.join(", "));
         conn.execute(&create_table_stmt, &[])?;
         Ok(conn.commit()?)
