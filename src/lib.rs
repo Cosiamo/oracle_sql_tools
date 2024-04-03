af8853913550d077e25b1e1b821a810f18464a64
Joshua Becnel <jcbecnel@gmail.com>
2024-04-03 10:47:48 -0500

    implemented options for FormatData

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,16 @@ pub mod utils;
 pub trait FormatData {
     fn fmt_data(self) -> FormattedData;
 }
+impl FormatData for FormattedData { fn fmt_data(self) -> FormattedData { self } }
 
 macro_rules! impl_fmt_data {
-    ($data:ident, $enum_type:ident) => {
+    ($data:ty, $enum_type:ident) => {
         impl FormatData for $data {
             fn fmt_data(self) -> FormattedData { $enum_type(self.into()) }
         }
     };
 }
 
-impl FormatData for FormattedData { fn fmt_data(self) -> FormattedData { self } }
 impl FormatData for &[u8] {
     fn fmt_data(self) -> FormattedData {
         let clone_on_write_string = String::from_utf8_lossy(self);
@@ -35,7 +35,7 @@ impl FormatData for Vec<u8> {
         STRING(utf8_string)
     }
 }
-impl FormatData for &str { fn fmt_data(self) -> FormattedData { STRING(self.into()) } }
+impl_fmt_data!(&str, STRING);
 impl_fmt_data!(String, STRING);
 impl_fmt_data!(i8, INT);
 impl_fmt_data!(i16, INT);
@@ -45,11 +45,61 @@ impl_fmt_data!(f32, FLOAT);
 impl_fmt_data!(f64, FLOAT);
 impl_fmt_data!(NaiveDateTime, DATE);
 
+macro_rules! impl_fmt_data_option {
+    ($data:ty, $enum_type:ident) => {
+        impl FormatData for $data {
+            fn fmt_data(self) -> FormattedData {
+                match self {
+                    Some(val) => $enum_type(val.into()),
+                    None => FormattedData::EMPTY,
+                }
+            }
+        }
+    };
+}
+
+impl FormatData for Option<&[u8]> {
+    fn fmt_data(self) -> FormattedData {
+        match self {
+            Some(val) => {
+                let clone_on_write_string = String::from_utf8_lossy(val);
+                let utf8_string = clone_on_write_string.replace(|c: char| !c.is_ascii(), "");
+                STRING(utf8_string)
+            },
+            None => FormattedData::EMPTY,
+        }
+    }
+}
+impl FormatData for Option<Vec<u8>> {
+    fn fmt_data(self) -> FormattedData {
+        match self {
+            Some(val) => {
+                let utf8_string = String::from_utf8(val)
+                    .map_err(|non_utf8| 
+                        String::from_utf8_lossy(non_utf8.as_bytes()).into_owned()
+                    )
+                    .unwrap();
+                STRING(utf8_string)
+            },
+            None => FormattedData::EMPTY,
+        }
+    }
+}
+impl_fmt_data_option!(Option<&str>, STRING);
+impl_fmt_data_option!(Option<String>, STRING);
+impl_fmt_data_option!(Option<i8>, INT);
+impl_fmt_data_option!(Option<i16>, INT);
+impl_fmt_data_option!(Option<i32>, INT);
+impl_fmt_data_option!(Option<i64>, INT);
+impl_fmt_data_option!(Option<f32>, FLOAT);
+impl_fmt_data_option!(Option<f64>, FLOAT);
+impl_fmt_data_option!(Option<NaiveDateTime>, DATE);
+
 // =========================
 pub trait PrepData<T: FormatData> {
     type Prep;
 
-    fn prep_data(self, conn: Connection) -> Self::Prep;
+    fn prep_data(self, connection: Connection) -> Self::Prep;
 }
 
 impl<T: FormatData> PrepData<T> for Vec<Vec<T>> {
